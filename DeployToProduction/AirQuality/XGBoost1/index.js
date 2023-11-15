d1ddf3f2da62ebf9f01bf88420d85645e163ae17
Javkhaa4 <146233620+Javkhaa4@users.noreply.github.com>
2023-11-15 13:36:13 -0600

    Update index.js

--- DeployToProduction/AirQuality/XGBoost1/index.js
+++ DeployToProduction/AirQuality/XGBoost1/index.js
@@ -5,22 +5,12 @@ async function runExample() {
     var x = [];
 
      x[0] = document.getElementById('box1').value;
-     x[1] = document.getElementById('box2').value;
-     x[2] = document.getElementById('box3').value;
-     x[3] = document.getElementById('box4').value;
-     x[4] = document.getElementById('box5').value;
-     x[5] = document.getElementById('box6').value;
-     x[6] = document.getElementById('box7').value;
-     x[7] = document.getElementById('box8').value;
-     x[8] = document.getElementById('box9').value;
-     x[9] = document.getElementById('box10').value;
-     x[10] = document.getElementById('box11').value;
-     x[11] = document.getElementById('box12').value;
 
-    let tensorX = new ort.Tensor('float32', x, [1, 12] );
+
+    let tensorX = new ort.Tensor('float32', x, [1, 1] );
     let feeds = {float_input: tensorX};
 
-    let session = await ort.InferenceSession.create('xgboost_AirQuality_ort.onnx');
+    let session = await ort.InferenceSession.create('xgboost_Classification_AirQuality_ort.onnx');
     
    let result = await session.run(feeds);
    let outputData = result.variable.data;
